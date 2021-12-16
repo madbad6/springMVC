e44e91df1304411a79090640f11c390aaf77a424
madbad6 <madbad6@yandex.ru>
2021-12-16 17:24:39 +0300

    new commit calc

--- src/main/java/com/madbad/springmvc/controller/FirstController.java
+++ src/main/java/com/madbad/springmvc/controller/FirstController.java
@@ -1,11 +1,10 @@
 package com.madbad.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
@@ -13,15 +12,33 @@ public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false)String name,
                             @RequestParam(value = "surname", required = false)String surname){
-
         System.out.println("Hello, "+name+" "+surname);
-
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodByePage(){
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action, Model model){
+        double result = 0;
+
+        switch (action){
+            case "+":result = a + b;
+            break;
+            case "-":result = a - b;
+                break;
+            case "/":result = (double) a / b;
+                break;
+            case "*":result = a * b;
+                break;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 }
 
